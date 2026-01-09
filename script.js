6554d1787ddd9ddbf98f605091ba4ea8495a40ee
Sofiane Delloue <sofianedelloue@Sofianes-MacBook-Pro.local>
2026-01-09 16:59:18 +0100

    Optimize homepage with responsive thumbnails

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const models = [
         id: "alina",
         name: "ALINA",
         mainImage: "models photos/16a2b21b-ddc6-4086-9b40-11913c41fdf3 1.png",
+        thumbnail: "models photos/alina_thumb.png",
         images: ["models photos/alina_fullbody_4.png", "models photos/alina_side_3.png"],
         stats: { height: "178cm", bust: "80cm", waist: "60cm", hips: "88cm", shoes: "39" },
         skills: "Runway, Editorial, Dance"
@@ -11,6 +12,7 @@ const models = [
         id: "leonid",
         name: "LEONID",
         mainImage: "models photos/41a6a2eb-17d5-493e-9d1c-650fc2dbc692 1.png",
+        thumbnail: "models photos/leonid_thumb.png",
         images: ["models photos/leonid_fullbody_1.png", "models photos/leonid_side_1.png"],
         stats: { height: "186cm", chest: "96cm", waist: "76cm", hips: "94cm", shoes: "44" },
         skills: "Sports, Commercial, Acting"
@@ -19,6 +21,7 @@ const models = [
         id: "wan",
         name: "WAN",
         mainImage: "models photos/4cb4b68d-bf1e-4d81-80a0-05ff0a51f26d 1.png",
+        thumbnail: "models photos/wan_thumb.png",
         images: ["models photos/wan_fullbody_1.png", "models photos/wan_side_1.png"],
         stats: { height: "179cm", bust: "81cm", waist: "61cm", hips: "88cm", shoes: "40" },
         skills: "High Fashion, Art, Music"
@@ -27,6 +30,7 @@ const models = [
         id: "harper",
         name: "HARPER",
         mainImage: "models photos/6bdeefaf-7de6-4047-ae68-f4c5ad766373 1.png",
+        thumbnail: "models photos/harper_thumb.png",
         images: ["models photos/harper_fullbody_2.png", "models photos/harper_side_1.png"],
         stats: { height: "176cm", bust: "82cm", waist: "63cm", hips: "89cm", shoes: "38" },
         skills: "Editorial, Catalog, Swim"
@@ -35,6 +39,7 @@ const models = [
         id: "jacob",
         name: "JACOB",
         mainImage: "models photos/89435bcb-3d58-4757-a29a-3c706cfd2a00 2.png",
+        thumbnail: "models photos/jacob_thumb.png",
         images: ["models photos/jacob_fullbody_1.png", "models photos/jacob_side_1.png"],
         stats: { height: "188cm", chest: "98cm", waist: "78cm", hips: "96cm", shoes: "45" },
         skills: "Runway, Streetwear, Skate"
@@ -43,6 +48,7 @@ const models = [
         id: "dan",
         name: "DAN",
         mainImage: "models photos/c1535d08-8af6-4173-b388-85c2e1893412 1.png",
+        thumbnail: "models photos/dan_thumb.png",
         images: ["models photos/dan_fullbody_1.png", "models photos/dan_side_1.png"],
         stats: { height: "185cm", chest: "95cm", waist: "75cm", hips: "93cm", shoes: "43" },
         skills: "Classic, Suit, Commercial"
@@ -51,6 +57,7 @@ const models = [
         id: "colborn",
         name: "COLBORN",
         mainImage: "models photos/c77db1b4-d26f-40e5-9b24-bcf5e3e2237c 1.png",
+        thumbnail: "models photos/colborn_thumb.png",
         images: ["models photos/colborn_fullbody_1.png", "models photos/colborn_side_1.png"],
         stats: { height: "187cm", chest: "94cm", waist: "74cm", hips: "92cm", shoes: "44" },
         skills: "Avant-Garde, Editorial"
@@ -59,6 +66,7 @@ const models = [
         id: "arthur",
         name: "ARTHUR",
         mainImage: "models photos/d9ecf849-a84d-46d4-bc47-622d793b87cc 1.png",
+        thumbnail: "models photos/arthur_thumb.png",
         images: ["models photos/arthur_fullbody_1.png", "models photos/arthur_side_1.png"],
         stats: { height: "184cm", chest: "93cm", waist: "73cm", hips: "91cm", shoes: "42" },
         skills: "Runway, Print, Acting"
@@ -67,6 +75,7 @@ const models = [
         id: "esmeralda",
         name: "ESMERALDA",
         mainImage: "models photos/newbreed 5 1.png",
+        thumbnail: "models photos/esmeralda_thumb.png",
         images: ["models photos/esmeralda_fullbody_1.png", "models photos/esmeralda_side_1.png"],
         stats: { height: "177cm", bust: "79cm", waist: "59cm", hips: "87cm", shoes: "39" },
         skills: "High Fashion, Dance, Video"
@@ -75,6 +84,7 @@ const models = [
         id: "jing",
         name: "JING",
         mainImage: "models photos/newbreed 8 1.png",
+        thumbnail: "models photos/jing_thumb.png",
         images: ["models photos/jing_fullbody_1.png", "models photos/jing_side_1.png"],
         stats: { height: "180cm", bust: "80cm", waist: "60cm", hips: "88cm", shoes: "40" },
         skills: "Editorial, Runway, Performance"
@@ -94,7 +104,7 @@ function renderGrid() {
         card.onclick = () => openProfile(model.id);
 
         // Encode URI component for paths with spaces
-        const imagePath = encodeURI(model.mainImage);
+        const imagePath = encodeURI(model.thumbnail);
 
         card.innerHTML = `
             <div class="model-name">${model.name}</div>
